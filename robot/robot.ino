6cb165f7c975e4d84080355af125411ae1dd1ca3
AlainRobitaille <alain.robitaille.0@gmail.com>
2012-03-04 09:30:15 +0800

    Update robot/robot.ino

--- robot/robot.ino
+++ robot/robot.ino
@@ -1,134 +1,137 @@
+
 /*To control the rover, Copy and paste the code below into the Arduino software*/ 
 // Test
 int E1 = 6; //M1 Speed Control 
 int E2 = 5; //M2 Speed Control 
 int M1 = 8; //M1 Direction Control 
 int M2 = 7; //M2 Direction Control 
-int ENEyePin; // EN pin for the eyes
-const int analogInPin1 = A0;  // Analog input pin that the potentiometer is attached to
-const int analogInPin2 = A1;  // Analog input pin that the potentiometer is attached to
-const int analogInPin3 = A2;  // Analog input pin that the potentiometer is attached to
-const int analogInPin4 = A3;  // Analog input pin that the potentiometer is attached to
-const int analogOutPin = 9; // Analog output pin that the LED is attached to
 
-int sensorValue1 = 0;        // value read from the pot
-int sensorValue2 = 0;        // value read from the pot
-int sensorValue3 = 0;        // value read from the pot
-int sensorValue4 = 0;        // value read from the pot
-
+int turnMultiplier = 1300; //ms par 90degré de rotation
+float accelerationModificator = 1.3; //7.75cm par sec. 1000ms/775 = 1.3
+int leftspeed = 255;  //255 is maximum speed  
+int rightspeed = 255; 
 
 void setup(void) 
 { 
-  int i; 
-  for(i=5;i<=8;i++) 
-  pinMode(i, OUTPUT); 
+  //int i; 
+  //for(i=5;i<=8;i++) 
+  //pinMode(i, OUTPUT); 
   Serial.begin(9600);
-  pinMode(ENEyePin, OUTPUT);      // sets the digital pin as output
-  digitalWrite(ENEyePin, HIGH);
+  Serial.println("Start;");
+}
 
-} 
 void loop(void) 
-{ 
-//  Serial.print("Ready to rock\n");  
-  
+{  
   while (Serial.available() < 1) {} // Wait until a character is received 
-  char val = Serial.read(); 
-  int leftspeed = 255;  //255 is maximum speed  
-  int rightspeed = 255; 
-  switch(val) // Perform an action depending on the command 
-  { 
+  char command = Serial.read();
+  int magnitude = readInt();
+  int finish = 0;
+  int collision = 0;
+  int timeCount = 0;
+  int timeLimit = 0;
+  
+  switch(command) // Perform an action depending on the command 
+  {
   case 'f'://Move Forward 
-    forward (leftspeed,rightspeed); 
+    forward();
+    timeLimit = magnitude * accelerationModificator;
     break; 
   case 'b'://Move Backwards 
-    reverse (leftspeed,rightspeed); 
+    reverse();
+    timeLimit = magnitude * accelerationModificator;
     break; 
   case 'l'://Turn Left 
-    left (leftspeed,rightspeed); 
+    left();
+    timeLimit = magnitude * turnMultiplier;
     break; 
   case 'r'://Turn Right 
-    right (leftspeed,rightspeed); 
+    right(); 
+    timeLimit = magnitude * turnMultiplier;
     break; 
   default: 
     stop(); 
     break; 
   }
-  int i = 0;
-  int collision = sense();
-  while (i <20 && ! collision){
-    delay(100);
-    collision = sense();
-    if(collision) {
-      stop();
-      Serial.print(val);
-      Serial.print(",");
-      Serial.print(i*100);
-      Serial.print(",");
-      Serial.print(collision);
-      Serial.print(";");
-      break;
+  
+  while (finish == 0) { // loop each 50ms
+    
+    timeCount = timeCount + 50;
+    delay(50);
+    
+    if(timeLimit !=0 && timeLimit < timeCount){
+      finish = 1;
     }
-    i++;
+    
+    //TODO détection de colision.
+    /*
+    if(collision){
+      collision = 1;
+      finish = 1;
+    }
+    */
   }
+  
   stop();
-
+  
+  //Envoyer un réponse
+  Serial.print(command);
+  Serial.print(",");
+  if(command == 'f' || command == 'b'){
+    Serial.print(int( timeCount / accelerationModificator ));
+  } else {
+    Serial.print(magnitude);
+  }
+  Serial.print(",");
+  Serial.print(collision);
+  Serial.println(";");
+  
 }
 
+int readInt(){
+  char buffer = int(Serial.read());
+  int value = 0;
+  
+  while (buffer != ';') {
+    value = value * 10;
+    value = value + buffer;
+    buffer = int(Serial.read());
+  }
+  return value;
+}
 
 void stop(void) //Stop 
 { 
   digitalWrite(E1,LOW); 
   digitalWrite(E2,LOW); 
-//  Serial.print("Stop");
+  Serial.print("Stop");
 } 
-void forward(char a,char b) 
+void forward() 
 { 
-  analogWrite (E1,a); 
+  analogWrite (E1,leftspeed); 
   digitalWrite(M1,LOW); 
-  analogWrite (E2,b); 
+  analogWrite (E2,rightspeed); 
   digitalWrite(M2,LOW); 
-//  Serial.print("J'avance");
+  Serial.print("J'avance");
 } 
-void reverse (char a,char b) 
+void reverse () 
 { 
-  analogWrite (E1,a); 
+  analogWrite (E1,leftspeed); 
   digitalWrite(M1,HIGH); 
-  analogWrite (E2,b); 
+  analogWrite (E2,rightspeed); 
   digitalWrite(M2,HIGH); 
-//  Serial.print("Je recule");
+  Serial.print("Je recule");
 } 
-void left (char a,char b) 
+void left () 
 { 
-  analogWrite (E1,a); 
+  analogWrite (E1,leftspeed); 
   digitalWrite(M1,HIGH); 
-  analogWrite (E2,b); 
+  analogWrite (E2,rightspeed); 
   digitalWrite(M2,LOW); 
 } 
-void right (char a,char b) 
+void right () 
 { 
-  analogWrite (E1,a); 
+  analogWrite (E1,leftspeed); 
   digitalWrite(M1,LOW); 
-  analogWrite (E2,b); 
+  analogWrite (E2,rightspeed); 
   digitalWrite(M2,HIGH); 
 }
-
-int outputValue = 0;        // value output to the PWM (analog out)
-
-int sense(void) // Sense objects
-{ 
-  
-  // read the analog in value:
-  sensorValue1 = analogRead(analogInPin1);            
-  sensorValue2 = analogRead(analogInPin2);            
-  sensorValue3 = analogRead(analogInPin3);            
-  sensorValue4 = analogRead(analogInPin4);            
-  // collision
-  if( (sensorValue1 + sensorValue2 + sensorValue3 + sensorValue4) / 4 > 600) {
-    stop();
-    // TODO: send serialized report
-    return 1;
-  } else {
-    return 0;
-  }
-}
-
