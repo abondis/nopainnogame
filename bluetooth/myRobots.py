f1f67f1b4438b44f77eb38bf3d7056e1fa775f37
Stéphane Duchesneau <stephane.duchesneau@savoirfairelinux.com>
2012-03-04 12:30:42 -0500

    travaux dimanche

--- bluetooth/myRobots.py
+++ bluetooth/myRobots.py
@@ -1,6 +1,5 @@
 import httplib
 import json
-import copy
 import time
 
 class MyRobots:
@@ -10,27 +9,29 @@ class MyRobots:
   def __init__(self, key="1647186ED7164FCE",url="bots.myrobots.com"):
     self.key=key
     self.url=url
+    self.entry_id=0
+    self.entry_id=self.read()["entry_id"]
 
-  def write(self, newdict, noReadFirst=False):
+  def write(self, newdict):
     self.conn = httplib.HTTPConnection(self.url)
     val=""
-    if noReadFirst:
-      current=newdict
-    else:
-      current=self.read()
-      for key in newdict.keys():
-        current[key] = newdict[key]
-    for key in current.keys():
-      val+= "&" + key + "=" + str(current[key])
+    for key in newdict.keys():
+      val+= "&" + key + "=" + str(newdict[key])
     val+="&status=true"
     self.conn.request("GET", "/update?key="+self.key+val)
-    while self.conn.getresponse().read() == '0':
+    returnid = self.conn.getresponse().read()
+    while returnid == '0':
       time.sleep(2)
       self.conn.request("GET", "/update?key="+self.key+val)
+      returnid = self.conn.getresponse().read()
+    self.entry_id=int(returnid)
 
   def read(self):
     self.conn = httplib.HTTPConnection(self.url)
     self.conn.request("GET","/channels/589/feed/last.json")
     data = json.loads(self.conn.getresponse().read())
+    if data["entry_id"] == self.entry_id: data["updated"] = "false"
+    else: data["updated"] = "true"
+    self.entry_id=data["entry_id"]
     return data
 
--- bluetooth/robotPoller.py
+++ bluetooth/robotPoller.py
@@ -5,11 +5,18 @@ import time
 #class RobotPoller:
 r=robotLib.Robot()
 mr=myRobots.MyRobots()
+debug=True
 
 while True:
   data=mr.read()
-  if data['field5'] == u'0':
-    r.write(data['field4'] + ';')
-    data['field5'] = u'1'
-    mr.write(data)
+  if debug: print "read data from cloud: ", data
+  if data['updated'] == "true" and data['field4'] != None: 
+    r.write(str(data['field4']) + ';')
+    if debug: print "wrote data to robot: ", data['field4'] + ';'
+  
+  robotdata=r.read()
+  if debug: print "read data from robot: ", robotdata
+  if robotdata != None:
+    mr.write({'field1': robotdata})
+    if debug: print "wrote data to cloud: ", {'field1': robotdata}
   time.sleep(2)
